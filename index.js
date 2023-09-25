d81cedb910799a74f964663095088f56a9420f83
LJ5O <75009579+LJ5O@users.noreply.github.com>
2023-09-25 14:08:53 +0200

    Added static folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./modules/initLogs.js')();
 
 // Créer une instance d'Express
 const app = express();
+server = require("http").createServer(app);
 
 // Configuration du moteur de template EJS
 app.set('view engine', 'ejs');
@@ -17,6 +18,8 @@ app.set('views', __dirname + '/views');
 app.use(morgan('dev'));
 
 // Routes
+app.use(express.static("views/static/"));
+
 app.get('/', (req, res) => {
   // Utiliser EJS pour rendre une vue (par exemple, views/index.ejs)
   res.render('index.ejs', { title: 'Mon Serveur Express' });
@@ -24,6 +27,6 @@ app.get('/', (req, res) => {
 
 // Démarrer le serveur sur le port 3000
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
