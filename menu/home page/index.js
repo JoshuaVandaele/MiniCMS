c5c33c05ddf22cbc3a3867d066126b3a58943981
SilverStardust <Yuristeelf@gmail.com>
2023-09-25 13:21:34 +0200

    home page

---
+++ menu/home page/index.js
@@ -0,0 +1,10 @@
+var template= new Vue({
+    el: '#template',
+    index: 1,
+    data: {
+        picture: "https://www.portboulognecalais.fr/media/3176-photo-montage-calais-boulogne-2021-4.png",
+        title: '',
+        text: '',
+        next: ''
+    }
+})
