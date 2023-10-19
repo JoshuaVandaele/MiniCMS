bf0f4cd5dc47d60d716828b55490063956bb48a9
SilverStardust <Yuristeelf@gmail.com>
2023-10-19 11:11:15 +0200

    feat: :sparkles: Key + Input type for each content in the json with the id in the URL
    Key + Input type for each content in the json with the id in the URL

--- src/views/EditTemplate.vue
+++ src/views/EditTemplate.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
         <div v-for="data in filteredJson" :key="data.id">
-            <div v-for="content in data.content" :key="content.id">
-                <div v-if="content == 'textbox'"><input type="textbox"></div>
-                <div v-if="content == 'number'"><input type="number" min="1"></div>
+            <div v-for="(content, key) in data.content" :key="key">
+
+                <label v-if = "typeof content !== 'object'"> {{ key }} </label>
+
+                <input type="textbox" v-if="content == 'textbox'">
+
+                <input type="number" min="1" v-if="content == 'number'">
+
                 <div v-if="typeof content === 'object'">
-                    <div v-for="language in content" :key="language.id">
-                        <div v-if="language == 'textarea'"><textarea></textarea></div>
+                    <div v-for="(language, langKey) in content" :key="langKey">
+                        <label>{{ langKey }}</label>
+                        <div ><textarea v-if="language == 'textarea'"></textarea></div>
                     </div>
                 </div>
+
                 <div v-if="content == 'img'"><input type="file" accept="image/png, image/jpeg"></div>
             </div>
         </div>
-    </div>
+
 </template>
 
 
@@ -28,40 +34,8 @@
 
         computed: {
             filteredJson() {
-                return this.myJson.filter(data => data.id == this.$route.params.id);
+                return this.myJson.filter(obj => obj.id == this.$route.params.id);
             },
         },
-
-        methods: {
-            saveTemplate() {
-                // Save the template on the local database
-                if (!localStorage.templates) {
-                    localStorage.templates = JSON.stringify({})
-                }
-                let templates = JSON.parse(localStorage.templates)
-                templates[this.template_id] = this.template_data
-                localStorage.templates = JSON.stringify(templates)
-            },
-
-            loadTemplate() {
-                // Load the template from the database
-                if (!localStorage.templates) {
-                    localStorage.templates = JSON.stringify({})
-                }
-                let templates = JSON.parse(localStorage.templates)
-
-                this.template_id = this.$route.params.id
-
-                if (!templates[this.template_id]) {
-                    templates[this.template_id] = {}
-                }
-
-                this.template_data = templates[this.template_id]
-            }
-        },
-
-        mounted() {
-            this.loadTemplate()
-        }
-    }
+    };
 </script>
